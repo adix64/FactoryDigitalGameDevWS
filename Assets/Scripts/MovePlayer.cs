3cb381e9432edbdeda9059d5e538192e8f488e98
Alex Dinu <adix64@gmail.com>
2020-10-05 17:39:55 +0300

    primul commit

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speedMultiplier = 1f; // multiplicatorul vitezei de deplasare a personajului
+    public Transform cameraTransform; // referinta catre componenta Transform a camerei (luata cu drag & drop...
+                                      // ... din ierarhie in slotul None)
+   
+    // functia apelata o singura data, la initializare
+    void Start()
+    {
+        // cod de initializare variabile
+    }
+
+    // apelata de N ori pe secunda, preferabil N >= 60 FPS
+    void Update()
+    {
+        float dx = Input.GetAxis("Horizontal"); // -1 pentru tasta A, 1 pentru tasta D, 0 altfel
+        float dz = Input.GetAxis("Vertical"); // -1 pentru tasta S, 1 pentru tasta W, 0 altfel
+
+        //calculam directia de deplasare, ca un vector normalizat (are lungime 1)
+        //suma axelor camerei suprapuse(inmultite) cu controalele(dx, dz)
+        Vector3 dir = (cameraTransform.right * dx + cameraTransform.forward * dz).normalized;
+        dir.y = 0f; // deplasare in planul orizontal (XoZ)
+        dir = dir.normalized; // lungime 1 pentru directii!
+        Vector3 offset = dir * Time.deltaTime * speedMultiplier; //calculam deplasamentul per frame
+        //Time.deltaTime ne ofera framerate independece: daca deplasamentul de la frame la frame nu este proportional
+        //cu timpul scurs intre 2 frameuri(deltaTime), viteza de miscare devine dependenta de FPS
+        transform.position += offset;//finally, adunam deplasamentul la pozitia personajului
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCameraCtrl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraCtrl : MonoBehaviour
+{
+    // https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Flight_dynamics_with_text_ortho.svg/1200px-Flight_dynamics_with_text_ortho.svg.png
+    float yaw = 0f; // unghiul cu axa verticala a lumii
+    float pitch = 0f; // unghiul cu axa orizontala a lumii
+    public Transform playerTransform; // referinta la target
+    public float distToTarget = 5f; // distanta pana la target
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        yaw += Input.GetAxis("Mouse X"); // adunam deplasamentul orizontal al mouselui
+        pitch += Input.GetAxis("Mouse Y"); // adunam deplasamentul vertical al mouselui
+
+        transform.rotation = Quaternion.Euler(pitch, yaw, 0f); // rotatia cu axele lumii
+        //pozitionarea camerei in jurul personajului:
+        transform.position = playerTransform.position - transform.forward * distToTarget;
+            // de la pozitia personajului, ne deplasam in spate distToTarget unitati
+    }
+}
